25a9ca3d1b9336ead7b538a01ef9421bd591a748
Rayan <rayan_muryell@hotmail.com>
2023-10-20 01:05:56 -0300

    image logo correction

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,19 +1,13 @@
 import * as React from 'react';
 import Toolbar from '@mui/material/Toolbar';
 
-import logo from '../NavBar/discordanimated.gif'
+import LogoPRM from 'next/image'
 import { Typography } from '@mui/material';
 
 export default function SearchAppBar() {
     return (
         <Toolbar style={{ backgroundColor: '#5e151b' }}>
-            <img
-                src={logo.src}
-                alt='Logo PRM'
-                width='80'
-                height='80'
-                style={{ margin: '5px' }}
-            />
+            <LogoPRM src='/discordanimated.gif' alt='Logo PRM' width='80' height='80'style={{ margin: '5px' }}/>
             <Typography sx={{ ml: 2, flex: 1, color: '#FFFFFF' }} variant="h5" component="div">
                 PRM - Database
             </Typography>
